9173486a4de7b14e49e1f52989e1b8250380dd72
FanMaker777 <myosotis465@gmail.com>
2025-06-10 13:21:35 +0900

    fix：stage3のエネミーファイル名を変更

--- firstshot/entities/enemies/stage3/__init__.py
+++ firstshot/entities/enemies/stage3/__init__.py
@@ -1,4 +1,4 @@
-from .enemy_trident_shooter import TridentShooter
-from .enemy_circle_shooter import CircleShooter
-from .enemy_charge_shooter import ChargeShooter
+from .trident_shooter import TridentShooter
+from .circle_shooter import CircleShooter
+from .charge_shooter import ChargeShooter
 from .boss import StageThreeBoss
