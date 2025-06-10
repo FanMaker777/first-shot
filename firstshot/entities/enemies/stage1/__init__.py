e8d95f6afc5eaf9ed626d3460faaab43fb41f167
FanMaker777 <myosotis465@gmail.com>
2025-06-11 00:12:36 +0900

    refactoring：ステージ1のエネミーのファイル名修正

--- firstshot/entities/enemies/stage1/__init__.py
+++ firstshot/entities/enemies/stage1/__init__.py
@@ -1,4 +1,4 @@
 from .boss import StageOneBoss
-from .enemy_around_shooter import AroundShooter
-from .enemy_player_shooter import PlayerShooter
-from .enemy_zigzag import Zigzag
+from .around_shooter import AroundShooter
+from .player_shooter import PlayerShooter
+from .zigzag import Zigzag
