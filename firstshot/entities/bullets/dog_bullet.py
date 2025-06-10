a68bc84026fc7e1813e01ae598b1acd92f0f4b84
FanMaker777 <myosotis465@gmail.com>
2025-06-10 15:55:12 +0900

    fix：ドッグ弾の当たり判定を画像通りに修正

--- firstshot/entities/bullets/dog_bullet.py
+++ firstshot/entities/bullets/dog_bullet.py
@@ -7,6 +7,13 @@ from firstshot.entities.bullets import Bullet
 
 class DogBullet(Bullet):
 
+    # 弾を初期化してゲームに登録する
+    def __init__(self, game, side, x, y, angle, speed):
+        """弾を初期化しゲームに登録する。"""
+        super().__init__(game, side, x, y, angle, speed)
+        # 当たり判定を専用に変更
+        self.hit_area = (0, 0, 16, 20)
+
     def draw(self):
         """ドッグ弾を描画する。"""
         pyxel.blt(self.x, self.y, self.game.special_bullet_image, 3, 98, 16, 20, COLOR_BLACK)
